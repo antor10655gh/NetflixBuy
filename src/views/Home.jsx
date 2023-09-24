c32569d1a7758a15ff280cb5b49983009a967055
CPO <cpo@CPOs-iMac.local>
2023-09-23 22:58:56 -0700

    add some change

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Hero from "../components/hero/Hero";
-import TrendingHero from "../components/trendinghero/TrendingHero";
-import BestSeller from "../components/category/bestseller/BestSeller";
-import SummerPlay from "../components/category/summerplay/SummerPlay";
-import GiftCards from "../components/category/giftCards/GiftCards";
 import BestSofware from "../components/category/bestSoftware/BestSoftware";
+import GiftCards from "../components/category/giftCards/GiftCards";
+import SummerPlay from "../components/category/summerplay/SummerPlay";
+import BestSeller from "../components/category/bestseller/BestSeller";
+import TrendingHero from "../components/trendinghero/TrendingHero";
 
 const Home = () => {
   return (
