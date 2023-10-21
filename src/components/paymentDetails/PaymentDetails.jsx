445c9e264f085ddaa438114dd5d77fd9ef5c80ee
antor100655gh <creativefreelancerantor@gmail.com>
2023-10-20 23:06:15 -0700

    add some change

--- src/components/paymentDetails/PaymentDetails.jsx
+++ src/components/paymentDetails/PaymentDetails.jsx
@@ -238,17 +238,41 @@ const PaymentDetails = () => {
   };
   let handleClose2 = () => {
     setOpen2(false);
+    let timerInterval;
     Swal.fire({
-      icon: "success",
-      title: "Payment Successful",
-      showConfirmButton: false,
-      timer: 2500,
+      title: "Your payment is processing",
+      html: "I will take few <b></b> milliseconds.",
+      timer: 2000,
+      timerProgressBar: true,
+      didOpen: () => {
+        Swal.showLoading();
+        const b = Swal.getHtmlContainer().querySelector("b");
+        timerInterval = setInterval(() => {
+          b.textContent = Swal.getTimerLeft();
+        }, 100);
+      },
+      willClose: () => {
+        clearInterval(timerInterval);
+      },
+    }).then((result) => {
+      /* Read more about handling dismissals below */
+      if (result.dismiss === Swal.DismissReason.timer) {
+        console.log("I was closed by the timer");
+      }
     });
     setTimeout(() => {
+      Swal.fire({
+        icon: "success",
+        title: "Payment Successful",
+        showConfirmButton: false,
+        timer: 2500,
+      });
+    }, 3500);
+    setTimeout(() => {
       navigate("/");
-    }, 3000);
+    }, 3600);
   };
-  const productCode = Math.floor(Math.random() * 100000000);
+  const productCode = Math.floor(Math.random() * 10000000000000000);
   const handleClick = (e) => {
     e.preventDefault();
 
@@ -483,7 +507,7 @@ const PaymentDetails = () => {
                 name="expirydate"
                 value={formData.expirydate}
                 onChange={handleExpiryDate}
-                className="w-full peer h-full   rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                className="w-full peer h-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 placeholder=" "
               />
               <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500  peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
